8d070e90e4a792edd3aaf309cae6aae06694b911
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-03-03 00:31:09 -0500

    create new instances of the meeting model using both rails console and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require 'ffaker'
+# require 'ffaker'
 
-10.times do
-  first_name = FFaker::Name.first_name
-  last_name = FFaker::Name.last_name
-  email = "#{first_name}.#{last_name}@gmail.com"
-  Speaker.create(first_name:first_name, last_name: last_name, email:email)
-end
+# 10.times do
+#   first_name = FFaker::Name.first_name
+#   last_name = FFaker::Name.last_name
+#   email = "#{first_name}.#{last_name}@gmail.com"
+#   Speaker.create(first_name:first_name, last_name: last_name, email:email)
+# end
+
+Meeting.create ({title:"Third meeting", agenda:"Discuss everything on the agenda", time: "4:00 pm"})
+Meeting.create ({title:"Fourth meeting", agenda:"Discuss everything on the agenda", time: "4:15 pm"})
